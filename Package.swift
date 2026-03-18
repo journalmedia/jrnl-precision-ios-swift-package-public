7bc1a59f453f9716362484bf09cc2e22a11c747e
fallonbrianmr <brian.fallon@gmail.com>
2026-03-18 21:29:20 +0000

    Release v0.0.2

--- Package.swift
+++ Package.swift
@@ -20,13 +20,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "JournalPrecisionDMP",
-            url: "https://jrnl-precision.s3.eu-west-1.amazonaws.com/mobile-sdks/ios/binaries/journal-precision-dmp/0.0.1/JournalPrecisionDMP.xcframework.zip",
-            checksum: "a03dedb7bee0d16cea3efe4202a7488de4aad3e3fe5dcdf0e481ff6a1e268be5"
+            url: "https://jrnl-precision.s3.eu-west-1.amazonaws.com/mobile-sdks/ios/binaries/journal-precision-dmp/0.0.2/JournalPrecisionDMP.xcframework.zip",
+            checksum: "7e62adbbc6936d663a150e9864998590a6cc8dbd53cb7c8c2617f92f84504803"
         ),
         .binaryTarget(
             name: "JournalPrecisionNativeNetwork",
-            url: "https://jrnl-precision.s3.eu-west-1.amazonaws.com/mobile-sdks/ios/binaries/journal-precision-native-network/0.0.1/JournalPrecisionNativeNetwork.xcframework.zip",
-            checksum: "8577d84b8eae2751176a12f44f1353739e6ee0f6056a2a00f7f93aaa21aeda8d"
+            url: "https://jrnl-precision.s3.eu-west-1.amazonaws.com/mobile-sdks/ios/binaries/journal-precision-native-network/0.0.2/JournalPrecisionNativeNetwork.xcframework.zip",
+            checksum: "812bc18a615878688d8d1437308da4d6c2cef1cb3e2671e61a80f65d64733910"
         )
     ]
 )
